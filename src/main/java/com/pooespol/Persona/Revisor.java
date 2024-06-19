303a51c00f1dcea645fb830291d7629814ba087d
Jesus Desintonio Leon <jedeleon@espol.edu.ec>
2024-06-18 21:13:58 -0500

    Update Revisor.java

--- src/main/java/com/pooespol/Persona/Revisor.java
+++ src/main/java/com/pooespol/Persona/Revisor.java
@@ -4,6 +4,52 @@ public class Revisor extend persona{
     private String contrasenia;
     private Usuario rol;
     private String user;
+
+    public Revisor (String nombre, String apellido, String correo, String nombreJournal, String contrasenia, Usuario rol, String user){
+        super(nombre, apellido,correo);
+        this.nombreJournal = nombreJournal;
+        this.contrasenia = contrasenia;
+        this.rol = rol;
+        this.user = user;
+    }
+
+    //Getters
+    
+    public String getNombreJournal(){
+        return nombreJournal;
+    }
+
+    public String getContrasenia(){
+        return contrasenia;
+    }
+
+    public Usiario getRol(){
+        return rol;
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    //Setters
+    
+    public void setNombreJournal(nombreJournal){
+        this.nombreJournal = nombreJournal:
+    }
+
+    public void setContrasenia(contrasenia){
+        this.contrasenia = contrasenia;
+    }
+
+    public void setRol(rol){
+        this.rol = rol;
+    }
+
+    public void setUser(user){
+        this.user = user;
+    }
+
+    //Metodos apartes
     
     
 }
