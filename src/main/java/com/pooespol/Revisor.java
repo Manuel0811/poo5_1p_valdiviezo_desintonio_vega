6d50afeb58da6ec1c8f6f7effd20e87311fabd21
Manuel0811 <vmvaldiv@espol.edu.ec>
2024-07-07 19:53:18 -0500

    Avance 7 Valdiviezo

--- src/main/java/com/pooespol/Revisor.java
+++ src/main/java/com/pooespol/Revisor.java
@@ -45,8 +45,71 @@ public class Revisor extends Persona{
     public void setUser(String user){
         this.user = user;
     }
+    public void setArticulosRevisados(int articulosRevisados){
+        this.articulosRevisados = articulosRevisados;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (this.getClass() == obj.getClass()){
+            Revisor revisor2 = (Revisor)obj;
+            if((nombre.equals(revisor2.getNombre())) && (apellido.equals(revisor2.getApellido())) && (correo.equals(revisor2.getCorreo())) && (especialidad.equals(revisor2.getEspecialidad())) && (user.equals(revisor2.getUser()))&&(contrasenia.equals(revisor2.getContrasenia()))&& (articulosRevisados==revisor2.getArticulosRevisados())&&(contraseniaCorreo.equals(revisor2.getcontraseniaCorreo())))
+                return true;
+        }
+        return false;    
+    }
+    
 
-    //Metodos apartes
+    public void validarArticulo(){
+        Scanner sc = new Scanner(System.in);
+        String mensaje =Aplicacion.leerCorreo(this);
+        System.out.println(mensaje);
+        System.out.println("Desea asignar el articulo a un Editor");
+        System.out.println("1. Aceptar");
+        System.out.println("2. Rechazar");
+        System.out.println("Escriba su opcion: ");
+        int opcion = sc.nextInt();
+        sc.nextLine();
+        switch (opcion){
+            case 1:
+                Articulo.aceptacion.add(true);
+                break;
+            case 2:
+                Articulo.aceptacion.add(false);
+                break;
+            default:
+                System.out.println("Opcion Invalida");
+        }
+        String[] articulo = mensaje.split("-");
+        ArrayList<String> palabrasClaves = new ArrayList<>();
+        String[] palabras = articulo[3].split(" ");
+        for(String e: palabras){
+            palabrasClaves.add(e);
+        }
+        String nombreAutor = articulo[4];
+        Autor autor = null;
+        for(Persona e: Aplicacion.personas){
+            if(e instanceof Autor){
+                Autor a1 = (Autor)e;
+                if(nombreAutor.equals(a1.getNombre()));
+                autor = a1;
+            }
+        }
+        Articulo art = new Articulo(articulo[0],articulo[1],articulo[2],palabrasClaves,autor);
+        Editor edi = null;
+        for(Persona e: Aplicacion.personas){
+            if(e instanceof Editor){
+                Editor editor = (Editor)e;
+                edi = editor;
+            }
+        }
+        Aplicacion.enviarCorreos(this, edi, "Publicaion de articulo", art);
+        sc.close();
+    }
     
     
 }
